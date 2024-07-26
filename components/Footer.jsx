2c06d9b8c8637cd44f8799930f536a3fb9afe9df
avantikasrivastav <105983351+avantika2195@users.noreply.github.com>
2024-07-26 20:05:23 +0530

    added components

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MDBFooter, MDBContainer } from 'mdb-react-ui-kit';
+
+export default function App() {
+  return (
+    <MDBFooter className='text-center text-white' style={{ backgroundColor: '#000' }}>
+      <MDBContainer className='p-4'></MDBContainer>
+
+      
+    </MDBFooter>
+  );
+}
